afa0b7262f9933c347d634fc8e95cb577e14787b
gondoh <gondoh@e-catchup.jp>
2014-08-01 12:11:39 +0900

    自動ログインのイベントを作成

---
+++ Event/AutoLoginControllerEventListener.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 管理機能へアクセスした際に自動的にログインする
+ * 取り扱い注意
+ */
+class AutoLoginControllerEventListener extends BcControllerEventListener {
+/**
+ * 登録イベント
+ * 
+ * @var array
+ */
+	public $events = array(
+		'startup'
+	);
+	
+/**
+ * startup
+ * 
+ * @param CakeEvent $event
+ */
+	public function startup(CakeEvent $event) {
+		
+		$Controller = $event->subject();
+		
+		// 管理画面にて、ログインしてない状態でアクセスしたら自動的に最も古いユーザにてログイン状態にする
+		// デフォルトであれば初期登録管理者
+		if(BcUtil::isAdminSystem() && !$Controller->BcAuth->user()) {
+			$authModelName = $Controller->BcAuth->authenticate['Form']['userModel'];
+			$authModel = ClassRegistry::init($authModelName);
+			$user = $authModel->find('first');
+			
+			$Controller->BcAuth->login($user[$authModelName]);
+		} 
+	}
+}
+
